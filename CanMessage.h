97ee0fe57e52863e8c2d4f2ef56f438b2b3465c5
ManasBTI <manas.kadam@bmwtechworks.in>
2026-02-03 06:19:39 +0000

    Changes to be committed: 	modified:   CanMessage.h

---
+++ CanMessage.h
@@ -0,0 +1,19 @@
+# pragma once
+
+#include <array>
+#include <cstdint>
+
+class CanMessage
+{
+public:
+
+    CanMessage(uint32_t id,  uint8_t dlc, const std::array<uint8_t, 8>& payload);
+
+    uint32_t getCanId() const;
+    uint8_t getdlc() const; 
+    const std::array<uint8_t, 8>& getpayload() const;
+private:
+    uint32_t canId_;
+    uint8_t dlc_;
+    std::array<uint8_t, 8> payload;
+}  // namespace CAN_Task
